d8e0a3b78484d472847ab3db353ff164682c8905
Dipti Kartikeya <Dipti.Kartikeya@atcltd.atc.org.nz>
2020-12-09 10:12:35 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WinFormLesson1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(42, 97);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(93, 41);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Click This";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.Location = new System.Drawing.Point(157, 97);
+            this.lbxOutput.MultiColumn = true;
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(120, 95);
+            this.lbxOutput.TabIndex = 1;
+            this.lbxOutput.SelectedIndexChanged += new System.EventHandler(this.lbxOutput_SelectedIndexChanged);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.Color.Tomato;
+            this.lblOutput.Location = new System.Drawing.Point(39, 155);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblOutput.TabIndex = 2;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(42, 196);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 311);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormLesson1
+{
+    public partial class Calculator : Form
+    {
+        int Age = 88;//global variable
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        { 
+            //lblOutput.Text = "Hello World";
+            // lblOutput.Text = txtInput.Text;
+            // lbxOutput.Items.Add(txtInput.Text);
+            string Name = "Tom";
+           
+            Age = Age + 1;
+            lbxOutput.Items.Add("Hello " + Name + " " + Age);
+            MessageBox.Show("Hello " + Name + " You are " + Age + " years old");
+        }
+
+        private void lbxOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbxOutput.Items.Clear();
+        }
+    }
+}
